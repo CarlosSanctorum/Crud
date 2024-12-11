532c0101a8e7548ebebb8ca1106d4fc246e362f6
CarlosSanctorum <carlos.santosgon77@gmail.com>
2024-12-10 23:21:16 -0300

    adicionando classes

--- src/Model/Pessoa.java
+++ src/Model/Pessoa.java
@@ -1,6 +1,8 @@
 package Model;
 
 public class Pessoa {
+
+    private int id;
     //add getters e setters
     private String nome;
     private String cargo;
@@ -11,10 +13,48 @@ public class Pessoa {
     public Pessoa() {
     }
 
-    public Pessoa(String nome, String cargo, String email, String senha) {
+    public Pessoa(int id, String nome, String cargo, String email, String senha) {
+        this.id = id;
         this.nome = nome;
         this.cargo = cargo;
         this.email = email;
-        this.senha = email;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId (int id){
+        this.id = id;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome (String nome){
+        this.nome = nome;
+
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 }
