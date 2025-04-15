52957262a31ef8041e14add48ebe15929b0ed347
CarlosSanctorum <carlos.santosgon77@gmail.com>
2025-04-14 21:36:08 -0300

    Testando commit - add classes

--- src/Controller/ProdutoCRUD.java
+++ src/Controller/ProdutoCRUD.java
@@ -1,4 +1,32 @@
 package Controller;
+import Model.Produto;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ProdutoCRUD {
+
+    private ArrayList<Produto> produtos = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void adicionarProduto() {
+        //aqui não poderia ficar um loop adicionando o id conforme eu vou add novos produtos?
+        System.out.println("Digite o ID do produto");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Nome produto");
+        String nome = scanner.nextLine();
+
+        System.out.print("Preço = ");
+        double preco = scanner.nextDouble();
+
+        System.out.print("Quantidade = ");
+        int quantidade = scanner.nextInt();
+
+        Produto produto = new Produto(id, nome, preco, quantidade);
+        produtos.add(produto);
+
+        System.out.println("Produto adicionado com sucesso!");
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,75 @@
 package Main;
 
+import java.util.Scanner;
+import Controller.ProdutoCRUD;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Olá, Mundo!");
 
-  
+        ProdutoCRUD produtoCRUD = new ProdutoCRUD();
+    //fazer um menu de escolhas aqui
+        //lembrar loop de repetição
+        int opcao = 0;
+        int objtc0 = 0;
+        String escolhacontinuacao;
+        while (opcao != 5) {
+            System.out.println("Menu");
+            System.out.println("1 - Created");
+            System.out.println("2 - Atualizar");
+            System.out.println("3 - Listar");
+            System.out.println("4 - Excluir");
+            System.out.println("5 - Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine(); //solução para o if e else não ser ignorado
+
+            if (opcao>5) {
+                System.out.println("Escolha uma opção valida!");
+                System.out.println("Quer continuar? y ou n");
+                escolhacontinuacao = scanner.nextLine();
+
+                if (escolhacontinuacao.equals( "S"))
+                    {
+                        System.out.println("Segue normalmente");
+                    }
+                        else
+                            opcao = 5;
+            }
+
+            switch (opcao) {
+                case 1:
+                System.out.println("Adicionar produto!");
+                    produtoCRUD.adicionarProduto();
+                break;
+
+                case 2:
+                    System.out.println("Alterar produto!");
+                    break;
+
+                case 3:
+                    System.out.println("Listar Produtos");
+                    break;
+
+                case 4:
+                    System.out.println("Excluir produto");
+                    System.out.println("Qual o ID do produto para ser excluido? :");
+                    objtc0 = scanner.nextInt();
+
+                    System.out.println("Excluir produto");
+
+
+                    break;
+
+                case 5:
+                    System.out.println("Encerrando o programma...");
+                    break;
+
+                default:
+                    System.out.println();
+            }
+
+        }
     }
 }
